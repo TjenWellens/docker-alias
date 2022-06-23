a75643337b6b91b8c50394462acd3dd54b68b507
Tjen Wellens <tjen.wellens@gmail.com>
2022-06-23 10:13:13 +0100

    (docker-alias) terraform GCP token instead of file
    https://jryancanty.medium.com/stop-downloading-google-cloud-service-account-keys-1811d44a97d9

--- shell/terraform.sh
+++ shell/terraform.sh
@@ -3,11 +3,12 @@
 ###
 
 #_not intended for manual use
-alias _tf_credentials='if [ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]; then echo ""; else echo "/workspace/${GOOGLE_APPLICATION_CREDENTIALS}"; fi'
+alias _tf_credentials='if [ -z "${GOOGLE_OAUTH_ACCESS_TOKEN}" ]; then echo ""; else echo "-e GOOGLE_OAUTH_ACCESS_TOKEN=\"${GOOGLE_OAUTH_ACCESS_TOKEN}\""; fi'
 
 # terraform plan
-alias terraform='docker run --rm -it --name terraform -v ~/.aws:/root/.aws -v ~/.config/gcloud:/root/.config/gcloud -v $(pwd):/workspace -w /workspace -e GOOGLE_APPLICATION_CREDENTIALS=$(_tf_credentials) hashicorp/terraform:light'
+alias terraform='docker run --rm -it --name terraform -v ~/.aws:/root/.aws -v ~/.config/gcloud:/root/.config/gcloud -v $(pwd):/workspace -w /workspace $(_tf_credentials) hashicorp/terraform:light'
 
 # tf
 #> terraform plan
-alias tf='docker run --rm -it --name terraform -v ~/.aws:/root/.aws -v ~/.config/gcloud:/root/.config/gcloud -v $(pwd):/workspace -w /workspace -e GOOGLE_APPLICATION_CREDENTIALS=$(_tf_credentials) --entrypoint /bin/sh hashicorp/terraform:light'
+alias tf='docker run --rm -it --name terraform -v ~/.aws:/root/.aws -v ~/.config/gcloud:/root/.config/gcloud -v $(pwd):/workspace -w /workspace $(_tf_credentials) --entrypoint /bin/sh hashicorp/terraform:light'
+
